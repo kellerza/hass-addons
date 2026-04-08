b70b6c9496964a2e816d56970e1034d61e077423
Johann Kellerman <kellerza@gmail.com>
2026-04-08 06:22:47 +0200

    d2

---
+++ build_prep.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+cd "$(dirname "$0")" || exit 1
+
+export ADDON="$1"
+export VER="$2"
+export PACKAGE="ha_addon"
+
+if [ -z "$VER" ] || [ -z "$ADDON" ]; then
+  echo "Error: Usage $0 <addon> <version>"
+  exit 1
+fi
+
+if [ ! -f "$ADDON/config.yaml" ]; then
+  echo "Error: Invalid addon [./$ADDON]. Please provide a valid add-on name."
+  exit 1
+fi
+
+mkdir -p $ADDON/$PACKAGE
+cp MANIFEST.in LICENSE README.md uv.lock $ADDON/$PACKAGE/
+cp -r ./src $ADDON/$PACKAGE/
+
+# update version in pyproject.toml
+sed "s/version =.*/version = \"$VER\"/g" pyproject.toml > $ADDON/$PACKAGE/pyproject.toml
+echo "$VER" > $ADDON/VERSION
